c0dfcf86cfe65092bd7fcc6683d973533460918c
Dominic Fernandez <dominic.fernandez1023@gmail.com>
2022-03-17 22:29:24 -0700

    update proposal

--- public/js/proposal.js
+++ public/js/proposal.js
@@ -5,10 +5,21 @@ const projectSchedule = [
     {deadline: "3/28/22", task: "HTML Skeleton for website.  Pages 1-3 of the Visualization Design should have the base HTML and basic JS functionality (no visualizations yet, just skeleton to hold the visualizations)"},
     {deadline: "3/29/22", task: "Set up CI/CD pipeline to auto deploy to cloud server (Firebase) using Github Actions"},
     {deadline: "4/6/22", task: "Alpha Release. Should include: Section 1, Section 2 (time progression + pop-up modal with visualizations), Section 3 (line chart with default data)"},
+    {deadline: "4/10/22", task: "Implement toggleable features for Section 2"},
+    {deadline: "4/15/22", task: "Implement toggleable features for Section 3"},
+    {deadline: "4/18/22", task: "Implement Optional Features"},
     {deadline: "4/20/22", task: "Beta Release. Should include everything in the Alpha release, but with the toggeable features and Optional features (if they are doable/reasonable to implement)"},
+    {deadline: "4/24/22", task: "Presentation rough draft (slides + speaker notes)"},
+    {deadline: "4/25/22", task: "Presentation rehearsal 1"},
+    {deadline: "4/27/22", task: "Presentation revised draft"},
+    {deadline: "4/29/22", task: "Presentation rehearsal 2"},
+    {deadline: "5/3/22", task: "Presentation final draft"},
+    {deadline: "5/5/22 - 5/8/22", task: "Presentation final rehearsal(s)"},
     {deadline: "5/9/22", task: "Final Project Presentation"},
+    {deadline: "5/11/22", task: "Project report outline"},
     {deadline: "5/16/22", task: "Project Report Draft"},
-    {deadline: "5/19/22", task: "Project report, slides, demo video, code & data, user manual"}
+    {deadline: "5/19/22", task: "Project report, slides, demo video, code & data, user manual"},
+    {deadline: "5/20/22", task: "Mandatory Friday night celebration 🎉"}
 ];
 
 const renderProjectSchedule = () => {
